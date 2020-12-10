d505bb0ed2ab1fe4795d4d23c5d5423c92320dbd
gman-code <67984470+gman-code@users.noreply.github.com>
2020-12-09 19:15:04 -0500

    Update api.rb

--- lib/cats_breeds/api.rb
+++ lib/cats_breeds/api.rb
@@ -15,20 +15,3 @@ class API
         end
     end 
 end
-
-# def self.get_breeds(cat)    
-#             url="https://api.thecatapi.com/v1/breeds"
-#             uri=URI(url)
-#             response=Net::HTTP.get(uri) 
-    
-#             info = JSON.parse(response)
-#             name = "Abyssinian"   
-#             temperament = "Active, Energetic, Independent, Intelligent, Gentle"   
-#             origin = "Egypt"  
-#             description = "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals"  
-#              cat.each do |i|    
-#                  cat.origin << cat[i] if (i.include? "origin") && cat[i] != "" && cat[i] != "" && cat[i] != nil  
-#              end 
-    
-# end
-    
