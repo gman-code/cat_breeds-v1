20bdd4ea1be6b88ec171cdc5188b82b26f13e85a
g-man <chemist2ever@gmail.com>
2020-12-10 19:38:14 -0500

    add line 48 next

--- lib/cli.rb
+++ lib/cli.rb
@@ -27,66 +27,68 @@ class CLI
     def no_breeds
         if @cats == []
             puts "Sorry no breeds with letter #{@breed_input[0]}"
-            run
-            user_selection
-        end
-    end
-        
-    def breed_selection
-        prompt
-        input = gets.strip.downcase 
-        while input != 'exit' 
-            if input.to_i > 0 && input.to_i <= @cats.length  
-                @cat = @cats[input.to_i - 1] 
-                display_cat_info
-                user_selection_1
-                break
-            elsif    
-                puts "Sorry that did not match any breeds!, Try again"  
-                print_breeds
-                breed_selection
-                break
+             run
+
             end
         end
         
-    end 
-    
-    def user_selection_1
-        puts"type 'exit' to exit" 
-        puts "type  'back' to go back to breeds lists!" 
-        puts "Please type 'start' to go back to the very beginning the letter selection!" 
-        input = gets.strip.downcase
-        if input == 'exit' 
-            return goodbye
-        elsif input == "back"
-            print_breeds 
-            breed_selection
-        elsif input == "start" 
-            run
-        else 
-            puts "incorrect input try again"
-            user_selection_1    
-        end
-    end
-           
-    def display_cat_info    
-        puts ""
-        puts "#{@cat.origin}"
-        puts "#{@cat.temperament}"
-        puts "#{@cat.description}"  
-        puts ""
-    end
-    def prompt  
-        puts ""
-        puts "Enter a number about your cat or 'exit' to exit."
-        puts ""
-    end     
-    def print_breeds 
-        @cats.each.with_index(1) do |c, i|
-        puts "#{i}. #{c.name}"
+        def breed_selection
+            prompt
+            input = gets.strip.downcase 
+            while input != 'exit' 
+                if input.to_i > 0 && input.to_i <= @cats.length  
+                    @cat = @cats[input.to_i - 1] 
+                    display_cat_info
+                    user_selection_1
+                    break
+                elsif    
+                    puts "Sorry that did not match any breeds!, Try again"  
+                    print_breeds
+                    breed_selection
+                   next
+                end
+            end
+            
         end 
-    end 
-    def goodbye
-        puts "Thanks for using Cat Breed have a great day!" 
-    end  
-end
+        
+        def user_selection_1
+            puts"type 'exit' to exit" 
+            puts "type  'back' to go back to breeds lists!" 
+            puts "Please type 'start' to go back to the very beginning the letter selection!" 
+            input = gets.strip.downcase
+            if input == 'exit' 
+                return goodbye
+            elsif input == "back"
+                print_breeds 
+                breed_selection
+            elsif input == "start" 
+                run
+            else 
+                puts "incorrect input try again"
+                user_selection_1    
+            end
+        end
+               
+        def display_cat_info    
+            puts ""
+            puts "#{@cat.origin}"
+            puts "#{@cat.temperament}"
+            puts "#{@cat.description}"  
+            puts ""
+        end
+        def prompt  
+            puts ""
+            puts "Enter a number about your cat or 'exit' to exit."
+            puts ""
+        end     
+        def print_breeds 
+            @cats.each.with_index(1) do |c, i|
+            puts "#{i}. #{c.name}"
+            end 
+        end 
+        def goodbye
+            puts "Thanks for using Cat Breed have a great day!" 
+        end  
+    end
+            
+        
