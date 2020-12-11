9c7f87f470a2ce281b924655f8f2928a6a0b2752
g-man <chemist2ever@gmail.com>
2020-12-10 23:20:49 -0500

    add line 19 in class Cat

--- lib/cats_breeds.rb
+++ lib/cats_breeds.rb
@@ -15,9 +15,9 @@ class Cat
         @@all
     end
     def self.display_cats_table(cats)
-        cats_table = TTY::Table.new(header: ["Id", "CAT"])
         cats.each.with_index(1) do |cat, i| 
-        cats_table << ["#{i}".blue, "#{cat.name}"]
+        cats_table = TTY::Table.new(header: ["Id", "CAT"], row:["#{i}".blue, "#{cat.name}"])
+        # cats_table << ["#{i}".blue, "#{cat.name}"]
     end
     puts cats_table.render(:unicode)
   end
