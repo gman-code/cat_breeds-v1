d244852620b7a64c3612ce73115850235fc081a3
g-man <chemist2ever@gmail.com>
2020-12-22 21:59:05 -0500

    update cli code

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,13 +1,16 @@
 class CLI
     def run
         welcome
+        menu
+    end 
+    def menu 
         user_selection
         get_breeds
         no_breeds
         print_breeds
         breed_selection
+
     end
-      
     def welcome 
         puts ""
         puts "Welcome To Cats Breeds"
@@ -16,86 +19,79 @@ class CLI
         puts "Enter a letter to get list of breeds, there is no breeds with these letters q,w,u,i,f,g,z,x,v"
         puts ""
     end
-        
     def user_selection
         @breed_input = gets.strip.downcase
         API.get_cats if Cat.all == []
     end
-        
-    
     def get_breeds
         @cats=Cat.all.select {|cat|cat.name[0].downcase == @breed_input[0]}
     end 
     def no_breeds
         if @cats == []
-            puts "start again sorry no breeds with letter #{@breed_input[0]}"
-             run
-            end
-        end
-
-        def breed_selection
-            prompt
-            input = nil
-            while input != 'exit' 
-                input = gets.strip.downcase 
-                if input.to_i > 0 && input.to_i <= @cats.length  
-                    @cat = @cats[input.to_i - 1] 
-                    @cat.display_cat_stats
-                    user_selection_1
-                    break
-                elsif input == "exit"
-                    return goodbye
-                    exit
-                else    
-                    puts "Sorry that did not match any breeds, please pick from option above!, Try again "  
-                    print_breeds
-                    breed_selection
-                   break
-                end
-            end
+            puts "Please select another letter, no breeds with letter: q,w,u,i,f,g,z,x,v.letter you typed: this is the letter #{@breed_input[0]}"
+            menu
+       end
+   end
             
-        end 
-         
-        def user_selection_1
-            puts "type 'exit' to exit" 
-            puts "type 'all' to get the description.[if_touched]"
-            puts "type  'back' to go back to breeds lists!" 
-            puts "Please type 'start' to go back to the very beginning the letter selection!" 
-            input = gets.strip.downcase
-            if input == 'exit' 
+   
+    def breed_selection
+        prompt
+        input = nil
+        while input != 'exit' 
+            input = gets.strip.downcase 
+            if input.to_i > 0 && input.to_i <= @cats.length  
+                @cat = @cats[input.to_i - 1] 
+                @cat.display_cat_stats
+                user_selection_1
+                break
+            elsif input == "exit"
                 return goodbye
-            elsif input == "back"
-                print_breeds 
+                exit
+            else    
+                puts "Sorry that did not match any breeds, please pick from option above!, Try again "  
+                print_breeds
                 breed_selection
-            elsif input == "start" 
-                run
-                elsif input == "all"
-                    puts @cat.description.yellow
-                    user_selection_1
-            else 
-                puts "incorrect input try again"
-                user_selection_1    
+                break
             end
         end
-               
-        def display_cat_info    
-            puts ""
-            puts "#{@cat.origin}"
-            puts "#{@cat.temperament}"
-            puts "#{@cat.description}"  
-            puts ""
+    end 
+    def user_selection_1
+        puts "type 'exit' to exit" 
+        puts "type 'all' to get the description.[if_touched]"
+        puts "type  'back' to go back to breeds lists!" 
+        puts "Please type 'start' to go back to the very beginning the letter selection!" 
+        input = gets.strip.downcase
+        if input == 'exit' 
+            return goodbye
+        elsif input == "back"
+            print_breeds 
+            breed_selection
+        elsif input == "start" 
+            run
+        elsif input == "all"
+            puts @cat.description.yellow
+            user_selection_1
+        else 
+            puts "incorrect input try again"
+            user_selection_1    
         end
-        def prompt  
-            puts ""
-            puts "Enter a number about your cat or 'exit' to exit."
-            puts ""
-        end     
-        def print_breeds 
-            Cat.display_cats_table(@cats)
-        end 
-        def goodbye
-            puts "Thanks for using Cat Breed have a great day!" 
-        end  
     end
-            
-        
+    def display_cat_info    
+        puts ""
+        puts "#{@cat.origin}"
+        puts "#{@cat.temperament}"
+        puts "#{@cat.description}"  
+        puts ""
+    end
+    def prompt  
+        puts ""
+        puts "Enter a number about your cat or 'exit' to exit."
+        puts ""
+    end     
+    def print_breeds 
+        Cat.display_cats_table(@cats)
+    end 
+    def goodbye
+        puts "Thanks for using Cat Breed have a great day!" 
+    end  
+end       
